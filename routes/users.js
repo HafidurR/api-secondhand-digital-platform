cb65e5a451d4fb159fb3d790ab3e3cecff88c656
fakhrizaluciha <fakhrizaluciha@gmail.com>
2022-06-15 21:56:16 +0700

    hapus index route

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const mailer = require()
 const userRoute = require('../controller/userController')
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
 router.post('/register', userRoute.register)
 
 module.exports = router;
