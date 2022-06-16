4fcec8073bd7c9b2bce9ac833516ebe7be6e641f
anggadsatria <angga.dsatria27@gmail.com>
2022-06-16 16:30:57 +0700

    add try catch and update error handling

--- controller/userController.js
+++ controller/userController.js
@@ -3,24 +3,33 @@ const model = require('../models');
 const {sendEmail} = require('../misc/mailer')
 
 const register = async (req, res) => {
-    const { email, nama, password } = req.body
-    const userData = {
-        email: email,
-        nama: nama,
-        password: password
-    }
-    const kirimEmail = await sendEmail(userData.email, userData.nama)
-    const tambahUser = await User.create(userData)
-        res.status(201).json({
+    try {
+        const { email, nama, password } = req.body
+        const userData = {
+            email: email,
+            nama: nama,
+            password: password
+        }
+        const tambahUser = await User.create(userData)
+        const kirimEmail = await sendEmail(userData.email, userData.nama)
+        return res.status(201).json({
             status: 'Success',
             data: {
                 nama: userData.nama
             }
         })
-    return res.status(400).json({
-        status: 'Error',
-        message: 'Nama sudah dipakai'
-    })
+    } catch (error) {
+        if (error.name === `SequelizeValidationError` || error.name === `SequelizeUniqueConstraintError`) {
+            return res.status(400).json({
+                status: 'Error',
+                message: error.errors[0].message
+            })
+        }
+        return res.status(400).json({
+            status: 'Error',
+            message: error
+        })
+    }
 }
 
 module.exports = {register}
