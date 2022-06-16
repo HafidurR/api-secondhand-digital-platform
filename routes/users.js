e01d59f847620a8c48918d15edf15ea533fa3dcc
anggadsatria <angga.dsatria27@gmail.com>
2022-06-16 16:48:48 +0700

    add login router

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userRoute = require('../controller/userController')
 
 router.post('/register', userRoute.register)
+router.post('/login', userRoute.login)
 
 module.exports = router;
