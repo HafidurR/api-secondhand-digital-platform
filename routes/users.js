ad4ca44951aae45eff8537abf4ab3fa638033c9c
HafidurR <deifah126@gmail.com>
2022-06-20 22:26:45 +0700

    edit routes user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userRoute = require('../controller/userController')
+const userRoute = require('../controller/userController');
 
 router.post('/register', userRoute.register)
 router.post('/login', userRoute.login)
