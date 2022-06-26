78869dcee8d6536080959cadd98c1e69119e9f80
anggadsatria <angga.dsatria27@gmail.com>
2022-06-26 18:50:04 +0700

    add RUD seller routers

--- routes/seller.routes.js
+++ routes/seller.routes.js
@@ -3,9 +3,9 @@ const transaction = require('../controller/sellerController')
 const userChecking = require('../misc/passport');
 
 router.get('/transaction', userChecking, transaction.getAllTransaction);
-// router.get('/transaction/:id', userChecking, transaction.getBuyerTransactionById);
+router.get('/transaction/:id', userChecking, transaction.getTransactionById);
 // router.post('/transaction', userChecking, transaction.createBuyerTransaction);
-// router.put('/transaction/:id', userChecking, transaction.updateBuyerTransaction);
-// router.delete('/transaction/:id', userChecking, transaction.deleteBuyerTransaction);
+router.patch('/transaction/:id', userChecking, transaction.updateTransaction);
+router.delete('/transaction/:id', userChecking, transaction.deleteTransaction);
 
 module.exports = router
