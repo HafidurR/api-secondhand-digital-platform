7700f65411daca8d87a7573f2b79f97b5c1122ac
anggadsatria <angga.dsatria27@gmail.com>
2022-06-26 18:49:42 +0700

    add RUD to seller transaction controller

--- controller/sellerController.js
+++ controller/sellerController.js
@@ -26,6 +26,79 @@ const getAllTransaction = async (req, res) => {
     }
 }
 
+// Seller accept or decline
+const getTransactionById = async (req, res) => {
+    try {
+        const jwt_payload = req.user
+        const reqId = req.params.id
+        const options = {
+            where: { id: reqId }
+        }
+
+        const findTransaction = await Transaksi.findOne(options)
+        if(!findTransaction) throw new Error ("Transaction not found")
+
+        return res.status(200).json({
+            status: "Success",
+            data: findTransaction
+        })
+
+    } catch (error) {
+        return res.status(404).json({
+            status: "Failed",
+            message: error.message
+        })
+    }
+}
+
+// Edit transaction and update transaction status accept/decline
+const updateTransaction = async (req, res) => {
+    try {
+        const jwt_payload = req.user;
+        const transactionId = req.params.id
+        const { status } = req.body
+        const updateData = { status_transaksi: status }
+        const findTransaction = await Transaksi.findOne({ where: { id: transactionId } })
+
+        if(!findTransaction) throw new Error ("Transaction cannot be updated")
+        await Transaksi.update(updateData, { where: { id: transactionId } })
+        findTransaction.reload()
+        return res.status(201).json({
+            status: "Success",
+            message: findTransaction
+        })
+    } catch (error) {
+
+        return res.status(404).json({
+            status: "Failed",
+            message: error.message
+        })
+    }
+}
+
+const deleteTransaction = async (req, res) => {
+    try {
+        const jwt_payload = req.user
+        const transactionId = req.params.id
+        const findTransaction = await Transaksi.findOne({ where: { id: transactionId } })
+        if(!findTransaction) throw new Error ("Transaction cannot be deleted")
+        await Transaksi.destroy({ where: { id: transactionId } })
+        return res.status(201).json({
+            status: "Success",
+            message: `Success deleted transaction`
+        })
+
+    } catch (error) {
+        return res.status(404).json({
+            status: "Failed",
+            message: error.message
+        })
+    }
+}
+
 module.exports = {
-    getAllTransaction
+    getAllTransaction,
+    getTransactionById,
+    updateTransaction,
+    deleteTransaction
 }
