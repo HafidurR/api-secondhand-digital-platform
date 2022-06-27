ba1888d0678b792c5f1f18156e706298fef44c79
anggadsatria <angga.dsatria27@gmail.com>
2022-06-27 19:45:27 +0700

    update kota

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ const cors = require('cors')
 const usersRouter = require('./routes/users');
 const categoryRouter = require('./routes/category.routes');
 const produkRouter = require('./routes/produk')
-<<<<<<< HEAD
 const buyerRouter = require('./routes/buyer.routes')
 const sellerRouter = require('./routes/seller.routes')
-=======
-const transactionRouter = require('./routes/transaction.routes')
 const kotaRouter = require('./routes/kota.routes')
->>>>>>> c998e6aff4704253498bae6a8ec139ebe57d7317
 
 const app = express();
 
@@ -29,12 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/produk', produkRouter)
 app.use('/categories', categoryRouter);
-<<<<<<< HEAD
 app.use('/seller', sellerRouter);
 app.use('/buyer', buyerRouter);
-=======
-app.use('/transaction', transactionRouter);
 app.use('/kota', kotaRouter);
->>>>>>> c998e6aff4704253498bae6a8ec139ebe57d7317
 
 module.exports = app;
