99f55134345777948934e2c9557646afbb5b3d58
HafidurR <deifah126@gmail.com>
2022-07-05 21:08:19 +0700

    add endpoint detail user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const userRoute = require('../controller/userController');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images');
+  },
+  filename: (req, file, cb) => {
+    const index = file.originalname.split('.').length;
+    cb(null, Date.now() + '.' + file.originalname.split('.')[index - 1]);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    )
+      return cb(null, true);
+    cb(null, false);
+    cb(new Error("Wrong filetype"));
+  },
+});
 
 router.post('/register', userRoute.register)
 router.post('/login', userRoute.login)
 router.get('/', userRoute.getAll)
-router.put('/:id', userRoute.updateUser)
+router.get('/:id', userRoute.getDetailUser)
+router.put('/:id', upload.single('foto'), userRoute.updateUser)
 
 module.exports = router;
