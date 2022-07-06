6182b8d9133b8b0a02a595a14fa15f15fd588cc6
fakhrizaluciha <fakhrizaluciha@gmail.com>
2022-07-06 20:54:20 +0700

    add gambar user

--- misc/multerImage.js
+++ misc/multerImage.js
@@ -1,10 +1,8 @@
 const multer = require('multer');
-const fs = require('fs')
 
 const storagePhoto = multer.diskStorage({
     destination: function (req, file, cb) {
         const imageLocation = './gambarProduk'
-        if(!fs.existsSync(imageLocation)) fs.mkdirSync(imageLocation, {recursive: true});
         cb(null, imageLocation)
     },
     filename: function (req, file, cb) {
--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+const uploadImage = require('../misc/multerImage')
 const userRoute = require('../controller/userController');
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './public/images');
-  },
-  filename: (req, file, cb) => {
-    const index = file.originalname.split('.').length;
-    cb(null, Date.now() + '.' + file.originalname.split('.')[index - 1]);
-  }
-});
-
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/jpeg"
-    )
-      return cb(null, true);
-    cb(null, false);
-    cb(new Error("Wrong filetype"));
-  },
-});
 
 router.post('/register', userRoute.register)
 router.post('/login', userRoute.login)
 router.get('/', userRoute.getAll)
 router.get('/:id', userRoute.getDetailUser)
-router.put('/:id', upload.single('foto'), userRoute.updateUser)
+router.put('/:id', uploadImage.single('gambar'), userRoute.updateUser)
 
 module.exports = router;
